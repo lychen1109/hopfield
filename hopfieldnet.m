30443c5a1e6b79f973b21356a908d05435bf2bc1
Luyi Chen <lychen1109@gmail.com>
2013-02-15 14:47:42 +0800

    initialization of W

--- hopfieldnet.m
+++ hopfieldnet.m
@@ -1,6 +1,8 @@
-function Cb=hopfieldnet(spimg,targetimg)
+function W=hopfieldnet(spimg,targetimg)
 %Hopfieldnet: calculate the best BDCT with hopfield network
 %expected output bdctimg
+%todos: 1) filter out those illegal Vxi; 2) add input params which can
+%shift colm
 
 %Initialization
 A=500;
@@ -22,4 +24,21 @@ tpmtarget=tpm1(bdcttarget,T,1);
 %calculate Cb
 Cb=tpmtarget-tpm;
 Cb=Cb(:);
+
+%initialize W
+W=zeros(2*T+1,25*128,(2*T+1)^2);
+for row=1:128
+    for col=1:25
+        for f=1:2*T+1
+            flag=f-T-1;
+            if bdctimg(row,(col-1)*5+3)+flag<0
+                continue;
+            else
+                D=tpmdiff(bdctimg,row,(col-1)*5+3,flag);
+            end
+            W(f,(row-1)*25+col,:)=reshape(D,1,1,(2*T+1)^2);
+        end
+    end
+end
+
 

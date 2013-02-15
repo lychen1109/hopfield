52ff376ade632420227ef7f928fdeb425bf8be64
Luyi Chen <lychen1109@gmail.com>
2013-02-15 13:19:59 +0800

    modify tpmdiff_test to test alternative tpm calculation

--- tpmdiff_test.m
+++ tpmdiff_test.m
@@ -6,16 +6,24 @@ bdctimg=blkproc(img,[8 8],@dct2);
 bdctimg=abs(round(bdctimg));
 T=4;
 tm=tpm1(bdctimg,T,1);
-N=1000;%test time
+N=400;%test time
 result=zeros(N,1);
 
 for i=1:N
-    row=ceil(rand*128);
-    col=ceil(rand*128);
-    flag=ceil(rand*(2*T+1))-T-1;
     bdctnew=bdctimg;
-    bdctnew(row,col)=bdctnew(row,col)+flag;
+    tmsig=tm;
+    for row=1:128
+        for coli=1:25
+            %col=(coli-1)*5+3;
+            col=coli;
+            flag=ceil(rand*(2*T+1))-T-1;
+            tmsig=tmsig+tpmdiff(bdctnew,row,col,flag);
+            bdctnew(row,col)=bdctnew(row,col)+flag;            
+        end
+    end
     tmnew=tpm1(bdctnew,T,1);
-    result(i,1)=1-isequal(tmnew-tm,tpmdiff(bdctimg,row,col,flag));
+    resulti=isequal(tmnew,tmsig);
+    fprintf('test result is %d\n',resulti);
+    result(i,1)=1-resulti;
 end
 fprintf('failed test number is %d\n',sum(result));

bd9455110039c9151fc3b43f14c9c645b2a3a35d
Luyi Chen <lychen1109@gmail.com>
2013-03-10 21:41:29 +0800

    change distcal to support multi sizes of images

--- distcal.m
+++ distcal.m
@@ -2,24 +2,25 @@ function [dist1,dist2]=distcal(imgoris,targetimgs,bdctresults)
 %calculate result of distance change
 
 T=10;
-L=size(imgoris,1);
+[L,M]=size(imgoris);
+M=sqrt(M);
 dist1=zeros(L,1);
 dist2=zeros(L,1);
 for i=1:L
     imgori=imgoris(i,:);
-    imgori=reshape(imgori,128,128);
+    imgori=reshape(imgori,M,M);
     bdctori=blkproc(imgori,[8 8],@dct2);
     bdctori=abs(round(bdctori));
     tpmori=tpm1(bdctori,T,2);
     
     targetimg=targetimgs(i,:);
-    targetimg=reshape(targetimg,128,128);
+    targetimg=reshape(targetimg,M,M);
     bdcttarget=blkproc(targetimg,[8 8],@dct2);
     bdcttarget=abs(round(bdcttarget));
     tpmtarget=tpm1(bdcttarget,T,2);
     
     bdctresult=bdctresults(i,:);
-    bdctresult=reshape(bdctresult,128,128);
+    bdctresult=reshape(bdctresult,M,M);
     bdctresult=abs(bdctresult);
     tpmresult=tpm1(bdctresult,T,2);
     

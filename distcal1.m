aea9305d2fd130d695a59f06e8dd99e708a31977
Luyi Chen <lychen1109@gmail.com>
2013-03-14 10:46:37 +0800

    add input validation

--- distcal1.m
+++ distcal1.m
@@ -1,6 +1,17 @@
 function [dist1,dist2]=distcal1(spimg,targetimg,bdctresult,T)
 %calculate distance change of single image
 
+%reshape images
+if length(spimg(:))~=length(targetimg(:)) || length(spimg(:))~=length(targetimg(:))
+    fprintf('image size is not the same\n');
+    return
+end
+pixnum=length(spimg(:));
+imgwid=sqrt(pixnum);
+spimg=reshape(spimg,imgwid,imgwid);
+targetimg=reshape(targetimg,imgwid,imgwid);
+bdctresult=reshape(bdctresult,imgwid,imgwid);
+
 tpmopt=1;
 bdctsp=blkproc(spimg,[8 8],@dct2);
 bdctsp=abs(round(bdctsp));

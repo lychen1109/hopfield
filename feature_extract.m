fbe6286a2fc10ce3621f1f0395da64aaa7f81cf1
Luyi Chen <lychen1109@gmail.com>
2013-05-06 15:24:24 +0800

    new file feature_extract

---
+++ feature_extract.m
@@ -0,0 +1,18 @@
+function feat=feature_extract(T)
+%load feature from color images
+
+root='C:\data\CASIA1\Au';
+files=dir([root filesep '*.jpg']);
+N=length(files);
+feat=zeros(N,(2*T+1)^2);
+for i=1:N
+    rgbimg=imread([root filesep files(i).name]);
+    image=rgb2gray(rgbimg);
+    image=double(image);
+    bdctimg=blkproc(image,[8 8],@dct2);
+    bdctimg=abs(round(bdctimg));
+    tm=tpm1(bdctimg,T,2);
+    feat(i,:)=tm(:)';
+end
+
+

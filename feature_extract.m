cc40727c89836a2e26a428ee7add06ca1ae1ad31
Luyi Chen <lychen1109@gmail.com>
2013-06-26 18:47:52 +0800

    change to only include partial features

--- feature_extract.m
+++ feature_extract.m
@@ -1,18 +1,22 @@
-function feat=feature_extract(root,T)
+function [featp]=feature_extract(root,T)
 %load feature from color images
 
-%root='C:\data\uncompre\4cam_auth';
 files=dir([root filesep '*.tif']);
 N=length(files);
-feat=zeros(N,(2*T+1)^2);
+%feat=zeros(2*T+1,2*T+1,N);
+featp=zeros(2*T+1,2*T+1,N);
 for i=1:N
-    rgbimg=imread([root filesep files(i).name]);
-    image=rgb2gray(rgbimg);
-    image=double(image);
-    bdctimg=blkproc(image,[8 8],@dct2);
-    bdctimg=abs(round(bdctimg));
-    tm=tpm1(bdctimg,T,2);
-    feat(i,:)=tm(:)';
+%     rgbimg=imread([root filesep files(i).name]);
+%     image=rgb2gray(rgbimg);
+%     image=double(image);
+%     bdctimg=blkproc(image,[8 8],@dct2);
+%     bdctimg=abs(round(bdctimg));
+%     tm=tpm1(bdctimg,T,2);
+%     feat(:,:,i)=tm;
+    tm2=featextpartial(root,files(i).name,T);
+    featp(:,:,i)=tm2;
 end
+
+featp=reshape(featp,(2*T+1)^2,N)';
 
 

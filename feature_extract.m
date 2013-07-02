a98b2858f705b092522723aeaca7617da132f23f
Luyi Chen <lychen1109@gmail.com>
2013-07-02 13:42:02 +0800

    change feature_extract to load feature from cb channel

--- feature_extract.m
+++ feature_extract.m
@@ -1,22 +1,20 @@
-function [featp]=feature_extract(root,T)
+function feat=feature_extract(root,filelist,T)
 %load feature from color images
 
-files=dir([root filesep '*.tif']);
-N=length(files);
-%feat=zeros(2*T+1,2*T+1,N);
-featp=zeros(2*T+1,2*T+1,N);
+%files=dir([root filesep '*.tif']);
+N=length(filelist);
+feat=zeros(2*T+1,2*T+1,N);
 for i=1:N
-%     rgbimg=imread([root filesep files(i).name]);
-%     image=rgb2gray(rgbimg);
-%     image=double(image);
-%     bdctimg=blkproc(image,[8 8],@dct2);
-%     bdctimg=abs(round(bdctimg));
-%     tm=tpm1(bdctimg,T,2);
-%     feat(:,:,i)=tm;
-    tm2=featextbothpart(root,files(i).name,T);
-    featp(:,:,i)=tm2;
+    rgbimg=imread([root filesep filelist{i}]);
+    ycbcr=rgb2ycbcr(rgbimg);
+    cb=double(ycbcr(:,:,2));
+    %cr=double(ycbcr(:,:,3));
+    %bdctimg=blkproc(image,[8 8],@dct2);
+    %bdctimg=abs(round(bdctimg));
+    tm=tpm1(cb,T,2);
+    feat(:,:,i)=tm;
 end
 
-featp=reshape(featp,(2*T+1)^2,N)';
+feat=reshape(feat,(2*T+1)^2,N)';
 
 

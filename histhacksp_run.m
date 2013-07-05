2ef5cb7a439fce3b4df53e80bea8d241d5445ea9
Luyi Chen <lychen1109@gmail.com>
2013-07-05 18:15:00 +0800

    remove output of tm in histhacksp_run

--- histhacksp_run.m
+++ histhacksp_run.m
@@ -1,19 +1,18 @@
-function [cbspmod,tmnew]=histhacksp_run(cbtarget,cbsp,K,T,shift)
+function [cbspmod]=histhacksp_run(cbtarget,cbsp,K,T,shift)
 %run histhacksp to get result
 
 N=length(cbsp);
 cbspmod=cell(N,1);
-tmnew=zeros(2*T+1,2*T+1,N);
+
 parfor i=1:N
    imtarget=double(cbtarget{i});
    img=double(cbsp{i});
    selection=nodeselectionsp(img,K,shift);
    tic;
-   [img,tm]=histhacksp(img,imtarget,K,T,selection);
+   [img]=histhacksp(img,imtarget,K,T,selection);
    tt=toc;
    fprintf('No. %d image processed in %g sec\n',i,tt);
    cbspmod{i}=uint8(img);
-   tmnew(:,:,i)=tm;
 end
 
-tmnew=reshape(tmnew,(2*T+1)^2,N)';
+

c334c5bf8a150e3528e0fd4e9e4a2e05bc3a0e70
Luyi Chen <lychen1109@gmail.com>
2013-07-11 13:51:35 +0800

    new file imagematch

---
+++ imagematch.m
@@ -0,0 +1,66 @@
+function V=imagematch(dismat,opt)
+%match image based on their distance matric
+% the output of the function is a connection matrix of two image list
+
+%initialization
+A=opt.A;
+B=opt.B;
+C=opt.C;
+D=opt.D;
+u0=opt.u0;
+
+%calculate Tmat and I
+T11=ones(90,90);
+T11=T11-eye(90);
+diagcell=cell(1);
+diagcell{1}=T11;
+diagcell=repmat(diagcell,90,1);
+T1=blkdiag(diagcell{:});
+T2=zeros(90^2,90^2);
+for i=1:89
+    T2=T2+diag(ones(90^2-i*90,1),i*90);
+end
+T2=T2+T2';
+T3=ones(90^2,90^2);
+T4=dismat(:)*dismat(:)';
+Tmat=-A*T1-B*T2-C*T3-D*T4;
+N=90;
+I=N*C*ones(90^2,1);
+
+%initialize U
+u00=u0*artanh(-44/45);
+U=ones(90^2,1)*u00+(rand(90^2,1)*0.2-0.1)*u0;
+V=nodeg(U,u0);
+E=-0.5*V(:)'*Tmat*V(:)-V(:)'*I(:);
+fprintf('iter:0  E0=%g\n',E);
+
+iter=0;
+while 1
+    iter=iter+1;
+    idx=randperm(90^2);
+    for i=1:90^2
+        m=idx(i);
+        U(m)=Tmat(m,:)*V(:)+I(m);
+        V(m)=nodeg(U(m),u0);
+    end
+    Enew=-0.5*V(:)'*Tmat*V(:)-V(:)'*I(:);
+    fprintf('iter:%d  Enew=%g\n',iter,Enew);
+%     [fall,f1,f2,f3]=objfun(A,B,C,V,N,Cb,W,T1);
+%     fprintf('fall=%g, f1=%g, f2=%g, f3=%g\n',fall,f1,f2,f3);
+    if Enew>=E
+        break;
+    else
+        E=Enew;
+    end
+end
+
+V=reshape(V,90,90);
+V(V>0.5)=1;
+V(V<=0.5)=0;
+if ~isequal(sum(V),ones(1,90))
+    fprintf('Not permutation matrix\n');
+end
+
+
+
+
